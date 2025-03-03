1303e3eee460c7773c86f8c00ff42c6e652d99b0
s-A301-25 <s-A301-25@K-A301-25>
2025-03-03 19:40:57 +0100

    Dodaj pliki projektów.

---
+++ EShopService/Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace EShopService.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public string Ean { get; set; } = default!;
+        public decimal Price { get; set; }
+        public int Stock { get; set; } = 0;
+        public string Sku { get; set; }
+        public bool Deleted { get; set; } = false;
+        public DateTime Created_at { get; set; } = DateTime.UtcNow;
+        public Guid Created_by { get; set; }
+        public DateTime Uploaded_at { get; set; } = DateTime.UtcNow;
+        public Guid Updated_by { get; set; }
+    }
+}
